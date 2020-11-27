a3f0f59a828b534c58538c1fe8abef357466be03
Sekti Wicaksono <sektiwicaksono92@gmail.com>
2020-11-27 10:07:49 +0700

    add navigation control on sidebar nav

--- js/index.js
+++ js/index.js
@@ -1,9 +1,29 @@
 const get = id => document.getElementById(id);
 
-let user = get('user');
-let userBlock = get('user__block');
+const user = get('user');
+const userBlock = get('user__block');
 
+const realTime = get('realtime-head');
+const realTimeItem = get('realtime-item');
+
+const nav = get('nav');
+const openNav = get('open-nav');
+const closeNav = get('close-nav');
+
+// User Toggle
 user.addEventListener('click', function () {
     userBlock.classList.toggle('user__block--open');
 });
 
+// Menu Item Toggle
+realTime.addEventListener('click', function () {
+    realTimeItem.classList.toggle('item--open');
+});
+
+// Navigation Toggle
+openNav.addEventListener('click', function () {
+    nav.classList.add('sidebar__nav--open');
+});
+closeNav.addEventListener('click', function () {
+    nav.classList.remove('sidebar__nav--open');
+});
