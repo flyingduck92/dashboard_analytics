d177219b803c55b5d436196e3a68c29a621ddb70
Sekti Wicaksono <sektiwicaksono92@gmail.com>
2020-12-03 14:31:45 +0700

    adjust media to query 1100 and 1468 + chartJS

--- js/index.js
+++ js/index.js
@@ -27,3 +27,65 @@ openNav.addEventListener('click', function () {
 closeNav.addEventListener('click', function () {
     nav.classList.remove('sidebar__nav--open');
 });
+
+
+
+
+/* Chart.JS */
+// Fake Data - Total 46946
+var dataset = [
+    { label: "Mobile", count: 18778, color: "#72bbe1" },
+    { label: "Desktop", count: 28168, color: "#3ea6dd" }
+];
+
+var dataTotal = dataset.reduce((acc, data) => (acc += data.count), 0);
+var mobileCalculate = (dataset[0].count / dataTotal) * 100;
+var desktopCalculate = (dataset[1].count / dataTotal) * 100;
+var mobilePercentage = `${mobileCalculate.toFixed(1)}%`;
+var desktopPercentage = `${desktopCalculate.toFixed(1)}%`;
+
+// Top Counter
+var counter = document.getElementById("counterTotal");
+counter.innerHTML = `${dataTotal}`;
+// Percentage Mobile
+var counter = document.getElementById("percentage_mobile");
+counter.innerHTML = mobilePercentage;
+// Percentage Desktop
+var counter = document.getElementById("percentage_desktop");
+counter.innerHTML = desktopPercentage;
+
+// Chart JS Initialize
+var ctx = document.getElementById("myChart").getContext("2d");
+var myChart = new Chart(ctx, {
+    type: "pie",
+    data: {
+        labels: [dataset[0].label.toUpperCase(), dataset[1].label.toUpperCase()],
+        datasets: [
+            {
+                data: [dataset[0].count, dataset[1].count],
+                backgroundColor: [dataset[0].color, dataset[1].color],
+                borderWidth: 1
+            }
+        ]
+    },
+    options: {
+        tooltips: {
+            callbacks: {
+                label: function (tooltipItem, data) {
+                    let allData = data.datasets[tooltipItem.datasetIndex].data;
+                    let sumData = allData.reduce((memo, data) => (memo += data), 0);
+
+                    let tooltipLabel = data.labels[tooltipItem.index];
+                    let tooltipData = allData[tooltipItem.index];
+                    let tooltipPercentageCalc = (tooltipData / sumData) * 100;
+                    let tooltipPercentage = `${tooltipPercentageCalc.toFixed(1)}%`;
+
+                    return `${tooltipLabel} : ${tooltipData} (${tooltipPercentage})`;
+                }
+            }
+        },
+        legend: {
+            display: false
+        }
+    }
+});
